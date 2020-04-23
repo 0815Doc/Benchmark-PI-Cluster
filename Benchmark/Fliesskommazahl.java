016540d68d23770d43c0ee418c133671c99f8436
jh <joerg.hollandt@web.de>
2020-04-23 17:22:04 +0200

    unnötigen Konstruktor entfernt; Berechnung auf das Wesentliche vereinfacht

--- Benchmark/Fliesskommazahl.java
+++ Benchmark/Fliesskommazahl.java
@@ -1,28 +1,21 @@
 
 /**
- * Beschreiben Sie hier die Klasse Flie�kommazahl.
+ * Beschreiben Sie hier die Klasse Fliesskommazahl.
  * 
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Fliesskommazahl extends Benchmark
 {
-   double ergebnis1 = 0;
-   double ergebnis2 = 0;
-   
-   public Fliesskommazahl()
-   {
-        getAnzahlDurchlaeufe();
-   }
-    
+   double ergebnis = 0;
+       
    public void berechneFkz()
     {
         for (int i = 0; i < getAnzahlDurchlaeufe(); i++)
         {
-            ergebnis1 +=  0.123 * i;
             for (int j = 0; j < 100000; j++)
             {
-                ergebnis2 +=  0.123 * j;
+                ergebnis +=  0.123 * j;
             }
         }
         // System.out.println("Ergebnis1: "+ ergebnis1);
--- Benchmark/Ganzzahl.java
+++ Benchmark/Ganzzahl.java
@@ -5,28 +5,21 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
+
 public class Ganzzahl extends Benchmark
 {
-    long ergebnis1 = 0;
-    long ergebnis2 = 0;
-        
-    public Ganzzahl()
-    {
-        getAnzahlDurchlaeufe();        
-    }
+    long ergebnis = 0;
     
     public void berechneGz()
     {
         for (int i = 0; i < getAnzahlDurchlaeufe(); i++)
         {
-            ergebnis1 +=  i;
             for (int j = 0; j < 100000; j++)
             {
-                ergebnis2 +=  j;
+                ergebnis +=  j;
             }
         }
-        // System.out.println("Ergebnis1: "+ ergebnis1);
-        // System.out.println("Ergebnis2: "+ ergebnis2);
+        // System.out.println("Ergebnis1: "+ ergebnis);
         // try {Thread.sleep(1000); } catch(Exception ex) { /* !! DOUH !! */ }
     }
 }
